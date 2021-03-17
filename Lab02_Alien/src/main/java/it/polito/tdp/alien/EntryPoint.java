3bc97e8f0057d85250f6a5f2b34f2845870f7a0b
sbagpp <sabagiuseppe2000@gmail.com>
2021-03-17 11:10:29 +0100

    lab2 es 1

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/EntryPoint.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/EntryPoint.java
@@ -2,6 +2,8 @@ package it.polito.tdp.alien;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.model.DataTraslate;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,8 +14,11 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+    	FXMLLoader loader = new FXMLLoader (getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+        FXMLController controller = loader.getController();
+        DataTraslate model = new DataTraslate();
+        controller.setModel(model);
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,40 +1,95 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
-    
-    @FXML
-    private Button btnTranslate;
-    
-    @FXML
-    private Button btnReset;
+
+    @FXML // fx:id="txtInput"
+    private TextField txtInput; // Value injected by FXMLLoader
+
+    @FXML // fx:id="buttonTraslate"
+    private Button buttonTraslate; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+	private DataTraslate model;
 
     @FXML
-    void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
+    void translate(ActionEvent event) {
+    	String testoCompleto =  this.txtInput.getText();
+    	testoCompleto = testoCompleto.toLowerCase();
+    	String arrayTesto [] = testoCompleto.split(" ");
+    	if(testoCompleto.matches("[a-zA-Z\s?]+")==false) {
+    		this.txtResult.setText("si possono inserire solo caratteri alfabetici\n");
+    		return ;
+    	}
+    	//String arrayTesto [] = testoCompleto.split(" ");
+    	if(arrayTesto.length>2) {
+    		this.txtResult.setText("si possono inserire al più due parole\nuna la WordAlien e una la WordHeart\n");
+    		this.txtInput.clear();
+    		return ;
+    	}
+    	if(arrayTesto.length == 2) { //popolamenot BD
+    		boolean flag = true;
+    		try {
+				flag = this.model.addTraslate(arrayTesto[0], arrayTesto[1]);
+			} catch (ExeceptionDuplicateTraslate e) {
+				// TODO Auto-generated catch block
+				try {
+					this.txtResult.setText("Esiste una già una trduzione associata a tela WordAlien ed è:\n"+this.model.ricerca(arrayTesto[0])+"\n");
+				} catch (ExceptionTraduzineNonTrovata e1) {
+					// TODO Auto-generated catch block
+					this.txtResult.setText("parola non trovata\n");
+				}
+				this.txtInput.clear();
+				return;
+			}
+    		if(flag) {
+    			this.txtResult.setText("taduzione aggiunta corretamente\n");
+    		}
+    	}
+    	if(arrayTesto.length == 1) {
+    		try {
+				this.txtResult.setText(""+this.model.ricerca(arrayTesto[0])+"\n");
+			} catch (ExceptionTraduzineNonTrovata e1) {
+				// TODO Auto-generated catch block
+				this.txtResult.setText("parola non trovata\n");
+			}
+    		this.txtInput.clear();
+    		return ;
+    	}
+    	this.txtInput.clear();
     }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
-    }
-    
-    
-    @FXML
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert buttonTraslate != null : "fx:id=\"buttonTraslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        
     }
+
+	public void setModel(DataTraslate model) {
+		this.model = model;
+		
+	}
 }
